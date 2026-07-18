b44fbca54c6ffca2b523470badb5054f66b46e44
Gabriel Groppo <gabgroppo@gmail.com>
2026-07-18 14:53:43 -0300

    Initial Commit

---
+++ Calendar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Calendar
+//
+//  Created by Gabriel Groppo on 18/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
