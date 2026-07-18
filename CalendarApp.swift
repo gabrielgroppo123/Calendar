132c1a76ac51b8d664df1de3728f10ed14f6e0b5
Gabriel Groppo <gabgroppo@gmail.com>
2026-07-18 14:50:04 -0300

    Add files via upload

---
+++ CalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarApp.swift
+//  Calendar
+//
+//  Created by Gabriel Groppo on 18/07/26.
+//
+
+import SwiftUI
+
+@main
+struct CalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
