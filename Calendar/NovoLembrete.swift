6375f100f4a804957ba544eb7684d885f125652f
Gabriel Groppo <gabgroppo@gmail.com>
2026-07-20 17:53:49 -0300

    Tela de Novo Lembrete Feita

---
+++ Calendar/NovoLembrete.swift
@@ -0,0 +1,73 @@
+//
+//  NovoLembrete.swift
+//  Calendar
+//
+//  Created by Gabriel Groppo on 20/07/26.
+//
+
+import SwiftUI
+
+struct NovoLembrete: View {
+    @State var titulo: String = ""
+    @State var descricao: String = ""
+    var body: some View {
+        ZStack{
+            Rectangle()
+                .frame(width: 405, height: 820)
+                .foregroundStyle(Color(.systemGray5))
+                .cornerRadius(40)
+            VStack{
+                HStack{
+                    Image("Cancelar")
+                    Text("Novo lembrete")
+                        .padding(.horizontal, 20)
+                        .fontWeight(.medium)
+                        .font(Font.custom("SFPro", size: 32))
+                        .foregroundStyle(.primary)
+                    Image("Confirmar")
+                }
+                .padding(.top, 50)
+                
+                //Titulo
+                TextField("Título", text: $titulo)
+                    .padding(.horizontal, 12)
+                    .frame(width: 350, height: 66)
+                    .background(Color(.systemGray3))
+                    .cornerRadius(15)
+                    .padding(.vertical, 40)
+                    .font(Font.custom("SFPro", size: 25))
+                
+                
+                //Descricao
+                TextField("Descrição", text: $descricao)
+                    .padding(.horizontal, 12)
+                    .padding(.bottom, 60)
+                    .frame(width: 350, height: 136)
+                    .background(Color(.systemGray3))
+                    .cornerRadius(15)
+                    .font(Font.custom("SFPro", size: 25))
+                    .padding(.bottom, 36)
+                HStack{
+                    Text("Cor")
+                        .font(Font.custom("SFPro", size: 25))
+                        .foregroundStyle(Color(.systemGray))
+                        
+                    
+                    Circle()
+                        .frame(width: 44, height: 44)
+                        .foregroundStyle(Color(.systemGray))
+                        .padding(.trailing, 230)
+                }
+                    
+                Spacer()
+            }
+            
+        }
+        .ignoresSafeArea(.keyboard)
+        .padding(.top, 30)
+    }
+}
+
+#Preview {
+    NovoLembrete()
+}
